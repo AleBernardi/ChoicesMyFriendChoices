d9e32823ea92b54340eab6db488581898ff57342
Felipe Graff Ampolini <felipeampolini@unochapeco.edu.br>
2020-05-01 15:45:05 -0300

    Adicionada a tela e as funções para determinar um vencedor.com.br

--- Assets/Scripts/RevelaCarta.cs
+++ Assets/Scripts/RevelaCarta.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using static UnityEngine.ColorUtility;
+
 public class RevelaCarta : MonoBehaviour
 {
     public GameObject[] cartas;
@@ -11,6 +13,19 @@ public class RevelaCarta : MonoBehaviour
     public Sprite Rainha;
     public Sprite Rei;
     public Sprite Virada;
+    public Text texto;
+    public Text winnerText;
+    public Canvas winnerCanvas;
+    public Color corP1;
+    public Color corP2;
+
+    private int pontuacaoP1;
+    private int pontuacaoP2;    
+    private bool flagplayer=true;
+    private bool winnerP1=false;
+    private bool winnerP2=false;
+    private int countRounds = 0;
+
     public int qtdCartas = 3;
     public int pesoCarta1;
     public int pesoCarta2;
@@ -28,6 +43,14 @@ public class RevelaCarta : MonoBehaviour
     void Start()
     {
         Debug.Log("Start");
+
+        Debug.Log(flagplayer);
+
+        AtualizaPlayer(flagplayer);
+
+        pontuacaoP1=0;
+        pontuacaoP2=0;    
+
         escolha = new int[2];
         baralho = new int[qtdCartas];
         botoes = new Button[qtdCartas];
@@ -87,12 +110,81 @@ public class RevelaCarta : MonoBehaviour
     void ButtonClicked(int botao){
         if(botao == posAs) {
             botoes[botao].image.sprite = As;
+            botoes[botao].interactable = false;
+            if(flagplayer){
+                winnerP1 = true;
+            }else{
+                winnerP2 = true;
+            }
         } 
         if(botao == posRei) {
             botoes[botao].image.sprite = Rei;
+            botoes[botao].interactable = false;
+            if(flagplayer){
+                pontuacaoP1 = 1;
+            }else{
+                pontuacaoP2 = 1;
+            }
         } 
         if(botao == posRainha) {
             botoes[botao].image.sprite = Rainha;
+            botoes[botao].interactable = false;
+            if(flagplayer){
+                //vitoria do oponente
+                winnerP2 = true;
+            }else{
+                //vitoria do oponente
+                winnerP1 = true;
+            }
+        }
+        flagplayer = !flagplayer;
+        AtualizaPlayer(flagplayer);
+        countRounds++;
+        if(winnerP1){
+            finaliza(1);
+        }else if(winnerP2){
+            finaliza(2);
+        }else if(countRounds>=2){
+            if(pontuacaoP1 > pontuacaoP2){
+                finaliza(1);
+            }else{
+                finaliza(2);
+            }
+            //finaliza o jogo
+        }
+    }
+
+    void finaliza(int player){
+        texto.text = "";
+        foreach(Button botaum in botoes)
+            botaum.interactable = false;
+
+        switch(player){
+            case 1: winnerCanvas.gameObject.SetActive(true);
+                    winnerText.text = "<color=#"+ToHtmlStringRGB(corP1)+">Winner winner chicken dinner!\nJogador 1 venceu!!</color>";
+                    Debug.Log("Jogador 1 vence");
+                    break;
+            case 2: winnerCanvas.gameObject.SetActive(true);
+                    winnerText.text = "<color=#"+ToHtmlStringRGB(corP2)+">Winner winner chicken dinner!\nJogador 2 venceu!!</color>";
+                    Debug.Log("Jogador 2 vence");
+                    break;
+        }
+    }
+
+    void AtualizaPlayer(bool flag)
+    {
+        if(flag){
+            //mostra canvas jogador 1
+            texto.text = "<color=#"+ToHtmlStringRGB(corP1)+">É a vez do jogador 1!</color>";
+
+            //somar pontuação da carta ao jogador 1 aqui e após click inverte a flag
+            //pontuacaoP1 = //valor da carta selecionada
+        }else{
+            //mostra canvas jogador 2
+            texto.text = "<color=#"+ToHtmlStringRGB(corP2)+">É a vez do jogador 2!</color>";
+
+            //somar pontuação da carta ao jogador 2 aqui e após click inverte a flag
+            //pontuacaoP2 = //valor da carta selecionada
         }
     }
 }
